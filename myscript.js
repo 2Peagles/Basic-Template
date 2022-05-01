1f9da0b0101daf5e381e727736cd2e99aecf46a0
2Peagles <103866435+2Peagles@users.noreply.github.com>
2022-04-30 20:48:54 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,52 @@
+function checkDriverAge(age) {
+	if (Number(age) < 18) {
+		return "Sorry, you are too yound to drive this car. Powering off";
+	} else if (Number(age) > 18) {
+		return "Powering On. Enjoy the ride!";
+	} else if (Number(age) === 18) {
+		return "Congratulations on your first year of driving. Enjoy the ride!";
+	}
+}
+var list=["tiger", "cat", "bear","bird"];
+console.log(list[0]);
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.sort
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.push("kiwi")
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.slice[1]
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.reverse()
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+array2[1][1][0];
+
+{
+    username:"niall"
+    password:"onedirection"
+}
+var database = [ {
+    username:"niall",
+    password:"onedirection"
+}
+] 
+
+var newsfeed = [
+	{
+		username: "niall",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "harry",
+		timeline: "Can't wait to become a developer from home!"
+	},
+	{
+		username: "Mitch",
+		timeline: "lmao!"
+	}
+];
+
+var userNamePrompt =prompt("what's your username?");
+var passwordPrompt = prompt("what's your username?");
